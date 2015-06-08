fce99c8bb56e92525e09722af0d59f04f1eeadd6
Aakriti Shroff <aakriti@mit.edu>
2015-06-08 16:31:49 -0700

    step-15

---
+++ client/views/channel/channel.js
@@ -0,0 +1,53 @@
+//helper to find channel name from collection
+Template.channel.helpers({
+    messages: function() {
+        var _id = Router.current().params._id;
+        return Messages.find({
+            _channel: _id
+        });
+    },
+
+    channel: function() {
+        var _id = Router.current().params._id;
+        return Channels.findOne({
+            _id: _id
+        });
+    },
+
+    user: function() {
+        return Meteor.users.findOne({
+            _id: this._userId
+        });
+    },
+
+    time: function() {
+        return moment(this.timestamp).format('h:mm a');
+    },
+
+    date: function(messages) {
+        var dateNow = moment(this.timestamp).calendar();
+        var instance = Template.instance();
+        if (!instance.date || instance.date != dateNow) {
+            return instance.date = dateNow;
+        }
+    }
+});
+
+//event handler to insert message on enter and not on shift when user finishes typing
+Template.channel.events({
+    'keyup textarea': function(event, instance) {
+        if (event.keyCode == 13 && !event.shiftKey) { // Check if enter was pressed (but without shift).
+            var _id = Router.current().params._id;
+            var value = instance.find('textarea').value;
+            value = value.replace("\n", " \n"); // Markdown requires double spaces at the end of the line to force line-breaks.
+            instance.find('textarea').value = ''; // Clear the textarea.
+            Messages.insert({
+                _channel: _id,
+                message: value,
+                _userId: Meteor.UserId(), //userId to each message
+                timestamp: new Date() // Add a timestamp to each message.
+            });
+        }
+    }
+
+});
---
+++ client/views/home/home.js
@@ -0,0 +1,21 @@
+
+//helper to find all channels in home
+Template.home.helpers({
+    channels: function() {
+        return Channels.find();
+    }
+});
+
+// Event Handler that inserts the channel on creation
+Template.home.events({
+    'submit form': function(event, instance) {
+        // We dont want the browser to submit the form as don't want the form to reload the page.
+        event.preventDefault();
+        var name = instance.find('input').value;
+        instance.find('input').value = '';
+
+        Channels.insert({
+            name: name
+        });
+    }
+});
